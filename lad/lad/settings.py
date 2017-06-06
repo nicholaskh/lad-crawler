9b878d6884cd2c2d1f7c87217d0dafca9e1c815c
hzx4585 <519907066@qq.com>
2017-06-06 14:25:46 +0800

    mongodb存储

--- lad/lad/settings.py
+++ lad/lad/settings.py
@@ -64,9 +64,14 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'lad.pipelines.LadPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'lad.pipelines.LadPipeline': 300,
+}
+
+MONGO_HOST = "127.0.0.1"  # 主机IP
+MONGO_PORT = 27017  # 端口号
+MONGO_DB = "pipeline_db"  # 库名
+MONGO_COLL = "test"  # collection名
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
