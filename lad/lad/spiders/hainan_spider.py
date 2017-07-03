7a43b5597314919bfa37294a72b669e40249e24e
hzx4585 <519907066@qq.com>
2017-07-03 15:55:45 +0800

    海南完成

---
+++ lad/lad/spiders/hainan_spider.py
@@ -0,0 +1,41 @@
+#coding=utf-8
+import scrapy
+
+from lad.items import LadItem
+
+class newsSpider(scrapy.Spider):
+    name = "hainan"
+    start_urls = ['http://ga.hainan.gov.cn/28/']
+    text = ""
+
+    def parse(self, response):
+        for infoDiv in response.xpath('/html/body/table[2]/tr[2]/td/table/tr/td[3]/table[4]/tr[2]/td/table[3]/tr/td[2]/a'):
+            info_url = infoDiv.xpath('@href').extract_first()
+            n_url = "http://ga.hainan.gov.cn" + info_url
+            yield scrapy.Request(url=n_url, callback=self.parse_info)
+
+    def parse_info(self, response):
+        item = LadItem()
+
+        item["city"] = "海南"
+        item["news_type"] = "警方提示"
+        item["title"] = response.xpath('//*[@id="artibody"]/table/tr[1]/td/font/text()').extract_first()
+        item["time"] = '2015-9-24'
+
+        text_list = response.xpath('//*[@id="artibody"]/table/tr[4]/td')
+
+        if len(text_list) >=2:
+            for str_slt in text_list:
+                if str_slt.xpath('text()').extract_first() is None:
+                    self.text = self.text
+                else:
+                    self.text = self.text + str_slt.xpath('text()').extract_first()
+        else:
+            if text_list.xpath('text()').extract_first() is None:
+                self.text = self.text
+            else:
+                self.text = self.text + text_list.xpath('text()').extract_first()
+        item["text"] = self.text
+        self.text = ""
+
+        yield item
