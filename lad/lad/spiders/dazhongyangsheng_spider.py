eabee374758d0acacbdce826a2b536b37eb4e49b
hzx4585 <519907066@qq.com>
2017-07-21 21:19:54 +0800

    再次修改

--- lad/lad/spiders/dazhongyangsheng_spider.py
+++ lad/lad/spiders/dazhongyangsheng_spider.py
@@ -1,37 +1,38 @@
 #coding=utf-8
 import scrapy
 
-from lad.items import YangshengItem
+from lad.items import YangshengwangItem
 
 class newsSpider(scrapy.Spider):
     name = "dazhongyangshengwang"
-    start_urls = ['http://www.cndzys.com/zixun/']
+    districts = ['yinshi', 'yinshi', 'zhongyi', 'shenghuoyangsheng', 'yundong']
+    start_urls = ['http://www.cndzys.com/%s/' % x for x in districts]
     text = ""
 
     def parse(self, response):
-        if len(response.xpath('/html/body/div/div/div/a')) == 26:
+        if response.xpath('//*[@class=" paging"]/span')[-2].xpath('a/text()').extract_first().encode('utf-8') == '下一页':
             #判断是否是最后一页,不是的话执行下面逻辑
-            if len(response.url) == 28:
-                next_url = 'http://www.cndzys.com/zixun/index2.html'
-            else:
-                num = int(response.url.split('index')[1].split('.')[0])
-                next_url = 'http://www.cndzys.com/zixun/index' + str(num + 1) + ".html"
+            next_url = response.url.split('index')[0] + response.xpath('//*[@class=" paging"]/span')[-2].xpath('a/@href').extract_first()
             yield scrapy.Request(url=next_url, callback=self.parse)
 
-        for infoDiv in response.xpath('/html/body/div/div/div/a/@href')[0]:
+        for infoDiv in response.xpath('//*[@class="con_left"]/div/div/h4/a/@href'):
             n_url = 'http://www.cndzys.com' + infoDiv.extract()
             yield scrapy.Request(url=n_url, callback=self.parse_info)
 
     def parse_info(self, response):
-        item = YangshengItem()
+        item = YangshengwangItem()
 
-        item["web"] = "大众养生网"
+        item["module"] = "保健常识"
+        item["class_name"] = response.xpath('//*[@class="location"]/a/text()')[-2].extract()
+        item["class_num"] = len(response.xpath('//*[@class="location"]/a')) - 1
+        item["specific_name"] = response.xpath('//*[@class="location"]/a/text()')[-1].extract()
         item["title"] = response.xpath('/html/body/div/div/div/h1/text()').extract_first()
-        item["yangsheng_type"] = "养生资讯"
-        time_leng = len(response.xpath('/html/body/div/div/div/text()')[18].extract().strip())
-        item["time"] = response.xpath('/html/body/div/div/div/div/span/text()')[1].extract()[3:13]
+        item["source"] = '大众养生网'
+        item["source_url"] = response.url
+        item['image_urls'] = response.xpath('//*[@style="text-align:center;"]/a/img/@src').extract() #提取图片链接
+        item["time"] = response.xpath('//*[@class="info"]/span/text()')[1].extract()
 
-        text_list = response.xpath('/html/body/div/div/div/p/text()')
+        text_list = response.xpath('//*[@class="content_text"]/p/text()')
 
         for p_slt in text_list:
             if p_slt.extract() is None:
@@ -41,4 +42,8 @@ class newsSpider(scrapy.Spider):
         item["text"] = self.text
         self.text = ""
 
+        if len(response.xpath('//*[@class=" paging"]/a/text()')) > 0:
+            if response.xpath('//*[@class=" paging"]/a/text()')[-1].extract().encode('utf-8') == '下一页':
+                n_url = 'http://www.cndzys.com/' + response.xpath('//*[@class=" paging"]/a/@href')[-1].extract()
+                yield scrapy.Request(url=n_url, callback=self.parse_info)
         yield item
