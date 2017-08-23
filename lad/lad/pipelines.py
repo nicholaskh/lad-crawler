4234b2204cfa7f5a063df2744779e3d87c110eeb
hzx4585 <519907066@qq.com>
2017-08-23 14:00:54 +0800

    修改pipeline

--- lad/lad/pipelines.py
+++ lad/lad/pipelines.py
@@ -29,7 +29,7 @@ class LadPipeline(object):
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
         new_imgarray = []
-        if imageUrls != None and len(item['imageUrls']) != 0:
+        if item['imageUrls'] != None and len(item['imageUrls']) != 0:
             for image_url in item['imageUrls']:
                 list_name = image_url.split('/')
                 file_name = list_name[len(list_name)-1]#图片名称
