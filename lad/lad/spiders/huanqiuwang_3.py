1be3d1a95f338872fc53c418542c2d2d6f22fb6e
ZhaoLei <1175108017@qq.com>
2018-04-11 22:28:13 +0800

    修改环球网2,添加环球网3

--- lad/lad/spiders/huanqiuwang_3.py
+++ lad/lad/spiders/huanqiuwang_3.py
@@ -1,40 +1,32 @@
 #coding=utf-8
 import scrapy
 import json
-
+import re
 from ..items import DailyNewsItem
 from ..spiders.beautifulSoup import processText, processImgSep
 from datetime import datetime
 from .basespider import BaseTimeCheckSpider
 
 class newsSpider(BaseTimeCheckSpider):
-    name = "huanqiuwang_2"
-    dict_commens = {
-        'http://huanqiu.13322ty.com/huanqiu/lanqiu?pageNo=1&pageSize=30': '体育',
-        'http://huanqiu.13322ty.com/huanqiu/zuqiu?pageNo=1&pageSize=30': '体育'
-    }
-    start_urls = ['%s' % x for x in dict_commens.keys()]
+    name = "huanqiuwang_3"
+    start_urls = ['http://run.huanqiu.com/control/258/0/10?callback=']
 
     def parse(self, response):
-        data = json.loads(response.text.strip())
-        print("zagaodea********")
-        pages = data["list"]
+        data = json.loads(response.text.strip('(').strip(')'))
+        pages = data["results"][0]
         for page in pages:
-            if page["infoType"] != 1:
-                continue
-            req = scrapy.Request(url=page["templateUrl"], callback=self.parse_info)
+            req = scrapy.Request(url="http://run.huanqiu.com" + page["url"], callback=self.parse_info)
             m_item = DailyNewsItem()
-            class_name = "体育"
-            m_item['className'] = class_name
+            m_item['className'] = "体育"
             # 相当于在request中加入了item这个元素
             req.meta['item'] = m_item
             yield req
 
     def parse_info(self, response):
         item = response.meta['item']
-        time = response.xpath('//div[@class="time_text"]/text()').extract_first().split(' ')[0].split(u'：')[1]
+        time = re.findall('"publish" : "(.*?)"', response.text)[0]
         try:
-            time_now = datetime.strptime(time.split(' ')[0], '%Y-%m-%d')
+            time_now = datetime.fromtimestamp(int(time))
             self.update_last_time(time_now)
         except:
             print("Something Wrong")
@@ -42,21 +34,22 @@ class newsSpider(BaseTimeCheckSpider):
 
         if self.last_time is not None and self.last_time >= time_now:
             return
-        item["time"] = time
         item["source"] = "环球网"
-        title = response.xpath('//div[@class="qm_list_main"]/h2/text()').extract_first()
+        title = response.xpath('//h1[@class="title"]/text()').extract_first()
         if title is None:
             return
+        item["time"] = time_now.strftime('%Y-%m-%d')
         item["title"] = title
         item["sourceUrl"] = response.url
-        text_list = response.xpath('//div[@class="detail_main"]/p')
+        text_list = response.xpath('//div[@class="content"]/*')
         text = processText(text_list)
         item["text"] = text
         img_list = processImgSep(text_list)
         final_img_list = []
         for img in img_list:
-            if 'http' in img:
-                final_img_list.append(img)
+            if 'http' not in img:
+                img = "http:" + img
+            final_img_list.append(img)
         item['imageUrls'] = final_img_list
         if text.strip().replace("$#$", "") == "":
             return
