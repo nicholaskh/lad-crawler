6fce446e1d813d3f81f9c2f536aae9d51a7c3f78
ZhaoLei <1175108017@qq.com>
2018-05-15 19:34:37 +0800

    添加新华网军事，体育

---
+++ lad/lad/spiders/xinhuawang_military.py
@@ -0,0 +1,82 @@
+#coding=utf-8
+import scrapy
+import json
+import re
+
+from ..items import DailyNewsItem
+from ..spiders.beautifulSoup import processText, processImgSep
+from datetime import datetime
+from .basespider import BaseTimeCheckSpider
+
+class newsSpider(BaseTimeCheckSpider):
+    name = "xinhuawang_military"
+    start_urls = ['http://www.xinhuanet.com/mil/yaowen.htm',
+                  'http://www.xinhuanet.com/mil/zhuanti.htm',
+                  'http://www.xinhuanet.com/mil/zhongguo.htm',
+                  'http://www.xinhuanet.com/mil/shijie.htm',
+                  'http://www.xinhuanet.com/mil/guandian.htm',
+                  'http://www.xinhuanet.com/mil/ruidu.htm',
+                  'http://www.xinhuanet.com/mil/yuejunqing.htm',
+                  'http://www.xinhuanet.com/mil/guofangdongyuan.htm',
+                  'http://www.xinhuanet.com/mil/hangtianfangwu.htm',
+                  'http://www.xinhuanet.com/mil/junminronghe.htm']
+
+    def parse(self, response):
+        nid = re.findall('pageNid":\["(.*?)"', response.text)[-1]
+        url = "http://qc.wa.news.cn/nodeart/list?nid=" + nid + "&pgnum=1&cnt=30"
+        req = scrapy.Request(url=url, callback=self.parse_url)
+        yield req
+
+    def parse_url(self, response):
+        data = json.loads(response.text.strip('(').strip(')'))
+        times = []
+        urls = []
+        for row in data["data"]["list"]:
+            times.append(row["PubTime"].split(' ')[0])
+            url = row["LinkUrl"]
+            urls.append(url)
+
+        for time, url in zip(times, urls):
+            try:
+                time_now = datetime.strptime(time, '%Y-%m-%d')
+                self.update_last_time(time_now)
+            except:
+                print("Something Wrong")
+                break
+
+            if self.last_time is not None and self.last_time >= time_now:
+                break
+
+            req = scrapy.Request(url=url, callback=self.parse_info)
+
+            m_item = DailyNewsItem()
+            m_item['time'] = time
+            m_item['className'] = "国际"
+            # 相当于在request中加入了item这个元素
+            req.meta['item'] = m_item
+            yield req
+
+    def parse_info(self, response):
+        item = response.meta['item']
+        item["source"] = "新华网"
+
+        title = response.xpath('//h1[@id="title"]/text()').extract_first()
+        if title is None:
+            return
+        item["title"] = title
+        item["sourceUrl"] = response.url
+        # 修改了text_list
+        text_list = response.xpath('//div[@class="article"]/p')
+        text = processText(text_list).strip().replace("$#$", "")
+        if text == "":
+            return
+        item["text"] = text
+        # img_list = processImgSep(text_list)
+        # final_img_list = []
+        # for img in img_list:
+        #     if 'http' not in img:
+        #         img = response.url.rsplit('/', 1)[0] + '/' + img
+        #     final_img_list.append(img)
+        item['imageUrls'] = None
+
+        yield item
---
+++ lad/lad/spiders/xinhuawang_sports.py
@@ -0,0 +1,73 @@
+#coding=utf-8
+import scrapy
+import re
+
+from ..items import DailyNewsItem
+from ..spiders.beautifulSoup import processText, processImgSep
+from datetime import datetime
+from .basespider import BaseTimeCheckSpider
+
+class newsSpider(BaseTimeCheckSpider):
+    name = "xinhuawang_sports"
+    start_urls = ['http://sports.news.cn/zcfx.htm',
+                  'http://sports.xinhuanet.com/tycy.htm',
+                  'http://sports.news.cn/mls.htm',
+                  'http://sports.news.cn/zhty.htm',
+                  'http://sports.news.cn/zgzq.htm',
+                  'http://sports.news.cn/xhs.htm',
+                  'http://sports.news.cn/bxyd.htm']
+
+    def parse(self, response):
+        urls = response.xpath('//div[@class="rod"]/a/@href').extract()
+        valid_child_urls = list()
+        times = list()
+
+        for url in urls:
+            time = url.rsplit('/', 3)[-3] + "-" + url.rsplit('/', 3)[-2]
+            try:
+                time_now = datetime.strptime(time, '%Y-%m-%d')
+                self.update_last_time(time_now)
+                times.append(time)
+            except:
+                print("error time format")
+                continue
+
+            if self.last_time is not None and self.last_time >= time_now:
+                break
+            valid_child_urls.append(url)
+
+        for index, temp_url in enumerate(valid_child_urls):
+            req = scrapy.Request(url=temp_url, callback=self.parse_info)
+
+            hit_time = times[index]
+            m_item = DailyNewsItem()
+            m_item['time'] = hit_time
+            m_item['className'] = "体育"
+            # 相当于在request中加入了item这个元素
+            req.meta['item'] = m_item
+            yield req
+
+    def parse_info(self, response):
+        item = response.meta['item']
+        item["source"] = "新华网"
+
+        title = response.xpath('//div[@class="btt"]/h1/text()').extract_first()
+        if title is None:
+            return
+        item["title"] = title
+        item["sourceUrl"] = response.url
+        # 修改了text_list
+        text_list = response.xpath('//div[@class="content"]/p')
+        text = processText(text_list).strip().replace("$#$", "")
+        if text == "":
+            return
+        item["text"] = text
+        # img_list = processImgSep(text_list)
+        # final_img_list = []
+        # for img in img_list:
+        #     if 'http' not in img:
+        #         img = response.url.rsplit('/', 1)[0] + '/' + img
+        #     final_img_list.append(img)
+        item['imageUrls'] = None
+
+        yield item
